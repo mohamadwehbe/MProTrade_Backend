ce8f38d059670ee1c22514be648f559d2371acb0
se <wehbe148@gmail.com>
2021-07-24 20:15:20 +0300

    updated

---
+++ app/Http/Controllers/NotifyController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use App\Models\Customer;
+use App\Models\Message;
+use App\Models\Notification;
+use Illuminate\Support\Facades\DB;
+
+class NotifyController extends Controller
+{
+    //
+}
